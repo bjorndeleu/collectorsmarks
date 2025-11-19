07c5d797fa6fcb6e5a219353805e259634ba231f
bjorndeleu <collectorsmarks@outlook.com>
2025-11-19 22:11:39 +0100

    Create Nav.js

---
+++ app/Nav.js
@@ -0,0 +1,11 @@
+export default function Nav() {
+  return (
+    <nav>
+      <a href="/">Home</a>
+      <a href="/stamps">Stamps Database</a>
+      <a href="/research">Research</a>
+      <a href="/about">About</a>
+      <a href="/contact">Contact</a>
+    </nav>
+  );
+}
