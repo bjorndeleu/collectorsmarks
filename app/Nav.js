020fac4dccf6bf0f122d39703ddb2b1ff27d98c0
bjorndeleu <collectorsmarks@outlook.com>
2025-11-23 15:42:42 +0100

    Update Nav.js

--- app/Nav.js
+++ app/Nav.js
@@ -1,8 +1,9 @@
 export default function Nav() {
   return (
-    <nav>
+    <nav className="main-nav">
       <a href="/">Home</a>
       <a href="/stamps">Stamps Database</a>
+      <a href="/drawings">Drawings</a>
       <a href="/research">Research</a>
       <a href="/about">About</a>
       <a href="/contact">Contact</a>
